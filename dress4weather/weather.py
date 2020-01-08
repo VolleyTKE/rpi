aa73817b9661016181619ba8f796fb1bc53d5259
sigkillchris <chris.a.carr@gmail.com>
2020-01-07 21:24:51 -0600

    add sunset and sunrise.

--- dress4weather/weather.py
+++ dress4weather/weather.py
@@ -1,6 +1,6 @@
 import os
 import pyowm
-from datetime import datetime, timedelta
+from time import time, ctime
 
 OWM_API_KEY = os.environ.get('OWM_API_KEY')
 #print(OWM_API_KEY)
@@ -14,11 +14,16 @@ print('test')
 print(w.get_temperature('fahrenheit'))
 
 description = w.get_detailed_status()
-clouds = str(w.get_clouds())
+clouds = w.get_clouds()
 temperature = w.get_temperature()
 wind = w.get_wind()
 rain = w.get_rain()
+sunrise = w.get_sunrise_time()
+sunset = w.get_sunset_time()
 
-print('description: ' + description + "\r\n" + "cloud coverage: %" + clouds)
 
 
+print("sunrise: ",ctime(sunrise))
+print("sunset: ",ctime(sunset))
+if clouds < 20:
+	print('It should be sunny- GLASSES!')
